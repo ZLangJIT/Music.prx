047de290a21139c4f490a7ecefa931855d6397b2
ZLangJIT <smallville7123@gmail.com>
2024-11-13 12:02:40 +1000

    build

--- music2/make.sh
+++ music2/make.sh
@@ -1,6 +1,6 @@
 psp-g++ -I /usr/local/pspdev/psp/sdk/include -DNOEXIT -DFPM_MIPS -O2 -G0 -Wall -fno-pic -fno-exceptions -fno-rtti -c -o main.o main.cpp
 
-psp-g++ -D_PSP_FW_VERSION=371 -L./lib -L/usr/local/pspdev/psp/sdk/lib -Wl,-q,-T/usr/local/pspdev/psp/sdk/lib/linkfile.prx -nostartfiles *.o -o music2.elf
+psp-g++ -D_PSP_FW_VERSION=371 -L./lib -L/usr/local/pspdev/psp/sdk/lib -Wl,-q,-T/usr/local/pspdev/psp/sdk/lib/linkfile.prx -nostartfiles *.o -o -lpspkernel music2.elf
 
 psp-fixup-imports music2.elf
 psp-prxgen music2.elf music2.prx
