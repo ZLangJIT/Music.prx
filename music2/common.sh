94f00b44713cb2e8377808ce4763311801f1411d
ZLangJIT <smallville7123@gmail.com>
2024-11-14 02:44:12 +1000

    build

--- music2/common.sh
+++ music2/common.sh
@@ -1,6 +1,6 @@
 function compile() {
     echo "CC $1.c"
-    psp-gcc -I $PSPSDK/include -I $DIR/include -DNOEXIT -DFPM_MIPS -O2 -G0 -Wall -fno-pic -c -o $1.o $1.c || exit 1
+    psp-gcc -I $PSPSDK/include -I $PSPSDK/include/libc -I $DIR/include -DNOEXIT -DFPM_MIPS -O2 -G0 -Wall -fno-pic -c -o $1.o $1.c || exit 1
     printf -- "$1.o " >> $DIR/file_list
 }
 
@@ -11,7 +11,7 @@ function link() {
     compile exports
     
     echo "linking $name.elf"
-    psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $@ $(cat $DIR/before_libs) -lc -lpspkernel -lpspsysmem_kernel -lpspsysmem_user $(cat $DIR/after_libs) -lpspsdk -o $name.elf || exit 1
+    psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $@ $(cat $DIR/before_libs) -lpsplibc -lpspkernel -lpspsysmem_kernel -lpspsysmem_user $(cat $DIR/after_libs) -lpspsdk -o $name.elf || exit 1
     
     echo "GEN $name.prx"
     psp-fixup-imports $name.elf || exit 1
