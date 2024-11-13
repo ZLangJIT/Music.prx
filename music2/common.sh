2b0e898f9eaff711e3e006725631dba5fa782f8f
ZLangJIT <smallville7123@gmail.com>
2024-11-13 21:43:06 +1000

    build

--- music2/common.sh
+++ music2/common.sh
@@ -10,13 +10,26 @@ function compile() {
     printf "$1.o " >> $DIR/file_list
 }
 
-function link() {
+function link_u() {
     echo "GEN EXPORTS $1"
     psp-build-exports -b $DIR/exports.exp > exports.c
     compile_c exports
     
     echo "link ELF $1"
     psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $DIR/plugin.o $DIR/plugin_main.o -lstdc++ -lc -lpspkernel -lpspuser -lpspsdk -o $1.elf || exit 1
+    
+    echo "generating PRX $1"
+    psp-fixup-imports $1.elf || exit 1
+    psp-prxgen $1.elf $DIR/$1.prx || exit 1
+}
+
+function link_k() {
+    echo "GEN EXPORTS $1"
+    psp-build-exports -b $DIR/exports.exp > exports.c
+    compile_c exports
+    
+    echo "link ELF $1"
+    psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $DIR/plugin_main.o -lc -lpspkernel -lpspsdk -o $1.elf || exit 1
     
     echo "generating PRX $1"
     psp-fixup-imports $1.elf || exit 1
@@ -46,7 +59,7 @@ function build_plugin() {
     cd $DIR/plugins/$1 || exit 1
     emit_info_u music2_prx_plugin__$1 music2_prx_plugin__$1
     ./make.sh || exit 1
-    link $1 || exit 1
+    link_u $1 || exit 1
     cd $DIR
     mv -v $1.prx seplugins/music2/plugins/$1.prx
 }
