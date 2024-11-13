2bc71f7b64f1db23137393c36bd70ca848aae6bb
ZLangJIT <smallville7123@gmail.com>
2024-11-13 18:08:37 +1000

    build

--- download.sh
+++ download.sh
@@ -1,5 +1,8 @@
-wget -q --show-progress https://github.com/ZLangJIT/Music.prx/releases/download/music/music.prx -O prx/music_tmp1.prx || exit 1
-wget -q --show-progress https://github.com/ZLangJIT/Music.prx/releases/download/music/seplugins.zip || exit 1
+rm -rf prx
+mkdir prx
+wget -q --show-progress https://github.com/ZLangJIT/Music.prx/releases/download/music/music.prx -O prx/music_tmp.prx || exit 1
+wget -q --show-progress https://github.com/ZLangJIT/Music.prx/releases/download/music/seplugins.zip -O prx/seplugins.zip || exit 1
 cd prx
-unzip ../seplugins.zip
+unzip seplugins.zip
+cd ..
 ls -l prx
