a39e14c64c7c6bb17c22841868801180a6e365ca
ZLangJIT <smallville7123@gmail.com>
2024-11-13 20:23:07 +1000

    build

--- music2/common.sh
+++ music2/common.sh
@@ -1,3 +1,9 @@
+function compile_c() {
+    echo "CC $1.c"
+    psp-gcc -I $PSPSDK/include -I $DIR/include -DNOEXIT -DFPM_MIPS -O2 -G0 -Wall -fno-pic -c -o $1.o $1.c || exit 1
+    printf "$1.o " >> $DIR/file_list
+}
+
 function compile() {
     echo "CXX $1.cpp"
     psp-g++ -I $PSPSDK/include -I $DIR/include -DNOEXIT -DFPM_MIPS -O2 -G0 -Wall -fno-pic -fno-exceptions -fno-rtti -c -o $1.o $1.cpp || exit 1
@@ -7,7 +13,7 @@ function compile() {
 function link() {
     echo "build exports for $1"
     psp-build-exports -b $DIR/exports.exp > exports.c
-    compile exports
+    compile_c exports
     
     echo "link ELF $1"
     psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $DIR/plugin.o -lstdc++ -lc -lpspkernel -lpspuser -lpspsdk -o $1.elf || exit 1
