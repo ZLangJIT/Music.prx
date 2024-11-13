60910b70dfaaa057561d4d738dc433c016dbbf26
ZLangJIT <smallville7123@gmail.com>
2024-11-13 11:33:14 +1000

    build

--- download.sh
+++ download.sh
@@ -1,2 +1,3 @@
-#wget https://github.com/ZLangJIT/Music.prx/releases/download/music/music.prx -O music_tmp.prx
-wget https://github.com/ZLangJIT/Music.prx/releases/download/music/music2.prx -O music_tmp.prx
+wget -q --show-progress https://github.com/ZLangJIT/Music.prx/releases/download/music/music.prx -O music_tmp1.prx
+wget -q --show-progress https://github.com/ZLangJIT/Music.prx/releases/download/music/music2.prx -O music_tmp.prx
+ls -l *.prx
