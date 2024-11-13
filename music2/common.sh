afdefa170f5f2ad9e32121aa4aa52dd37bb06742
ZLangJIT <smallville7123@gmail.com>
2024-11-14 02:25:57 +1000

    build

--- music2/common.sh
+++ music2/common.sh
@@ -8,7 +8,7 @@ function link() {
     name=$1
     shift 1
     psp-build-exports -b $DIR/exports.exp > exports.c
-    compile_c exports
+    compile exports
     
     echo "linking $name.elf"
     psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $@ $(cat $DIR/before_libs) -lc -lpspkernel -lpspsysmem_kernel $(cat $DIR/after_libs) -lpspsdk -o $name.elf || exit 1
