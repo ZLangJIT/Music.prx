5ede06189a1b569224cd536942d7fa289f43a87a
ZLangJIT <smallville7123@gmail.com>
2024-11-13 15:04:31 +1000

    build

--- music2/common.sh
+++ music2/common.sh
@@ -5,7 +5,7 @@ function compile() {
 
 function link() {
     echo "link ELF $1"
-    psp-g++ -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles *.o $DIR/plugin.o -lpspkernel -lpspsdk -lc -o $1.elf || exit 1
+    psp-g++ -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles *.o $DIR/plugin.o -lpspkernel -lc -lpspsdk -o $1.elf || exit 1
     
     echo "generating PRX $1"
     psp-fixup-imports $1.elf || exit 1
