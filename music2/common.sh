8249ddd40575a243530c3ef375a5f1cf3ef54eeb
ZLangJIT <smallville7123@gmail.com>
2024-11-13 22:21:55 +1000

    build

--- music2/common.sh
+++ music2/common.sh
@@ -16,7 +16,7 @@ function link_u() {
     compile_c exports
     
     echo "link $1"
-    psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $DIR/plugin.o $DIR/plugin_main.o -lstdc++ -lc -lpspkernel -lpspuser -lpspsdk -o $1.elf || exit 1
+    psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $DIR/plugin.o -lstdc++ -lc -lpspkernel -lpspuser -lpspsdk -o $1.elf || exit 1
     
     echo "GEN $1.prx"
     psp-fixup-imports $1.elf || exit 1
