cc1c15ab963728a6294037ff975b027460192ca0
ZLangJIT <smallville7123@gmail.com>
2024-11-13 23:54:48 +1000

    build

--- music2/common.sh
+++ music2/common.sh
@@ -18,6 +18,8 @@ function link_k() {
     compile_c exports
     
     echo "link $name"
+    echo "link $name before libs $(cat $DIR/before_libs)"
+    echo "link $name after libs $(cat $DIR/after_libs)"
     psp-gcc -D_PSP_FW_VERSION=371 -L$DIR/lib -L$PSPSDK/lib -Wl,-q,-T$PSPSDK/lib/linkfile.prx -nostartfiles $(cat $DIR/file_list) $@ $(cat $DIR/before_libs) -lc -lpspkernel $(cat $DIR/after_libs) -lpspsdk -o $name.elf || exit 1
     
     echo "GEN $name.prx"
