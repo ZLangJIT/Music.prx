dd417d6958f70a44faf72a9a565cf219cc749bdf
ZLangJIT <smallville7123@gmail.com>
2024-11-13 14:40:57 +1000

    build

--- music2/main.cpp
+++ music2/main.cpp
@@ -1,9 +1,9 @@
 #include <plugin_main.h>
-struct main_thread;
-PRX_MAIN(main_thread);
 struct main_thread : public prx_thread {
     void run(void * arg) override {
         while(1)
           sceKernelDelayThreadCB(DELAY_THREAD_SEC*1);
     }
 };
+
+PRX_MAIN(main_thread);
--- music2/plugins/display/main.cpp
+++ music2/plugins/display/main.cpp
@@ -1,9 +1,8 @@
 #include <plugin_main.h>
-struct display_thread;
-PRX_MAIN(display_thread);
 struct display_thread : public prx_thread {
     void run(void * arg) override {
         while(1)
           sceKernelDelayThreadCB(DELAY_THREAD_SEC*1);
     }
 };
+PRX_MAIN(display_thread);
