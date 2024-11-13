b2673d3673d80fed508f26b276962269c55dbde2
ZLangJIT <smallville7123@gmail.com>
2024-11-13 14:39:35 +1000

    build

--- music2/plugin.cpp
+++ music2/plugin.cpp
@@ -12,13 +12,13 @@ int prx_thread_main(SceSize args, void *argp) {
 }
 
 void prx_thread::start(const char * thread_name, int8_t priority, void * arg) {
-    arg[0] = this;
-    arg[1] = arg;
+    this->arg[0] = this;
+    this->arg[1] = arg;
     id = sceKernelCreateThread(thread_name, prx_thread_main, THREAD_PRIORITY+priority, 0x4000, 0, NULL);
    	sceKernelStartThread(id, 1, arg);
 }
 
-void prx_thread::stop(const char * thread_name) {
+void prx_thread::stop() {
     sceKernelTerminateDeleteThread(id);
 }
 
